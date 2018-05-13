70a2f626513c7f9cc68eb34bfb3404e0a74d8a3a
Jim Crossley <jim@crossleys.org>
2018-05-13 09:09:03 -0400

    Cleanup AWS resources when trigger is deleted
    We can probably simplify this logic by updating the trigger resource
    with the bucket name and TopicArn on the CREATE

--- actions/feed.js
+++ actions/feed.js
@@ -1,12 +1,15 @@
 
 const AWS = require('aws-sdk');
 
-function main(args) {
+function initAws(args) {
   const accessKeyId = args.accessKeyId;
   const secretAccessKey = args.secretAccessKey;
   const region = args.region;
   AWS.config.update({region, credentials: {accessKeyId, secretAccessKey}});
+}
 
+function main(args) {
+  initAws(args);
   const lifecycleEvent = args.lifecycleEvent;
   if (lifecycleEvent === 'CREATE') {
     console.log("Creating trigger " + args.triggerName);
@@ -23,8 +26,7 @@ function triggerCreate(args) {
   if (!args.bucket) return {error: "Bucket name is required"};
 
   const bucket = args.bucket;
-  var trigger = args.triggerName.split('/');
-  trigger = trigger[trigger.length -1];
+  const trigger = triggerName(args);
   const endpoint = endpointUrl(args.webhookAction, trigger);
 
   const s3 = new AWS.S3();
@@ -64,20 +66,15 @@ function triggerCreate(args) {
       .then(_ => topicArn);
   }
   const configureBucketNotification = function(topicArn) {
-    const params = {
-      Bucket: bucket,
-      NotificationConfiguration: {
-        TopicConfigurations: [
-          {
-            Events: [
-              "s3:ObjectCreated:*"
-            ],
-            TopicArn: topicArn
-          }
-        ]
-      }
-    };
-    return s3.putBucketNotificationConfiguration(params).promise()
+    return s3.getBucketNotificationConfiguration({Bucket: bucket}).promise()
+      .then(data => {
+        data.TopicConfigurations.push({Events: [ "s3:ObjectCreated:*" ], TopicArn: topicArn});
+        const params = {
+          Bucket: bucket,
+          NotificationConfiguration: data
+        };
+        return s3.putBucketNotificationConfiguration(params).promise()
+      })
       .then(_ => topicArn);
   }
 
@@ -96,7 +93,43 @@ function triggerCreate(args) {
 }
 
 function triggerDelete(args) {
-  return {};
+  initAws(args);
+  const sns = new AWS.SNS();
+  const s3 = new AWS.S3();
+  const trigger = triggerName(args);
+
+  const reconfigureBucketNotification = function(topic) {
+    // Gnarly because we have to find our bucket in the bowels of
+    // the topic's policy. We could avoid this if the DELETE
+    // action passed the same bucket param we passed to CREATE
+    return sns.getTopicAttributes(topic).promise()
+      .then(data => {
+        const b = JSON.parse(data.Attributes.Policy).Statement[0].Condition.ArnLike['aws:SourceArn'].split(':');
+        const bucket = b[b.length - 1];
+        return s3.getBucketNotificationConfiguration({Bucket: bucket}).promise()
+          .then(data => {
+            data.TopicConfigurations = data.TopicConfigurations.filter(x => x.TopicArn != topic.TopicArn);
+            const params = {
+              Bucket: bucket,
+              NotificationConfiguration: data
+            };
+            return s3.putBucketNotificationConfiguration(params).promise()
+              .then(_ => topic);
+          });
+      });
+  }
+
+  return new Promise(function(resolve, reject) {
+    sns.listTopics().promise()
+      .then(data => data.Topics.find(x => x.TopicArn.match(':'+trigger+'$')))
+      .then(reconfigureBucketNotification)
+      .then(topic => sns.deleteTopic(topic).promise())
+      .then(data => resolve(data))
+      .catch(err => {
+        console.log(err);
+        reject(err);
+      });
+  });
 }
 
 function endpointUrl(webhookAction, trigger) {
@@ -104,6 +137,11 @@ function endpointUrl(webhookAction, trigger) {
   action[action.length - 1] = webhookAction;
   action = action.join('/');
   return process.env['__OW_API_HOST'] + '/api/v1/web' + action + '?trigger=' + trigger;
+}
+
+function triggerName(args) {
+  const x = args.triggerName.split('/');
+  return x[x.length - 1];
 }
 
 exports.main = main;
